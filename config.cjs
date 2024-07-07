35c279eadfabaa90767d9afcf80756b17e025b3e
disindu123 <disindudamsandu@gmail.com>
2024-07-07 16:35:37 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&F9zhKYjY",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&LJAmiXYm",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : true,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : true,
@@ -18,8 +18,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "DISINDU DAMSANDU",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "+94785781034",
+  OWNER_NAME: process.env.OWNER_NAME || "DISINDU-DAMSANDU",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94775704025",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : true, 
 };
