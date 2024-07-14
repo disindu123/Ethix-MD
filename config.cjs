8032f9b4a0c013925f2031241c2bdb7ddf3b21e4
disindu123 <disindudamsandu@gmail.com>
2024-07-14 16:16:37 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,7 +18,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "DISINDU-DAMSANDU",
+  OWNER_NAME: process.env.OWNER_NAME || "💫DISINDU-DAMSANDU💫",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "94775704025",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : true, 
